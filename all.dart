8963a01914c5796229b747b155f4ca88fecc66e1
linder <achielinder@gmail.com>
2024-04-02 22:07:16 +0300

    done

--- all.dart
+++ all.dart
@@ -15,7 +15,7 @@ void main() {
     print(num);
   }
 
-  // switch
+  // switch/case
   String studentMajor = 'database';
 
   switch (studentMajor) {
@@ -33,5 +33,43 @@ void main() {
       {
         print('You chose software engineering');
       }
+  }
+  // while loop
+  {
+    int number = 20;
+
+    while (number >= 10) {
+      print(number);
+      number--;
+    }
+  }
+  // odd or even
+  int number1 = 11;
+  if (number1 % 2 == 0) {
+    print('$number1 is an even number');
+  } else {
+    print('$number1 is an odd number ');
+  }
+
+  //  largest number
+  {
+    List<int> numbers = [10, 5, 20, 15, 8];
+    int maxNumber = numbers[0];
+
+    for (int i = 1; i < numbers.length; i++) {
+      if (numbers[i] > maxNumber) {
+        maxNumber = numbers[i];
+      }
+    }
+
+    print('The largest number in the list is: $maxNumber');
+  }
+
+  // try catch block
+  try {
+    var result = 10 ~/ 0;
+    print(result);
+  } catch (e) {
+    print('Error: $e');
   }
 }
